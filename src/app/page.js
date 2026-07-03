7f3fa8b1b2571005bbeae21178769ee13ff5c76b
Lawrence Caulker <locaulker@gmail.com>
2026-07-03 09:59:38 -0400

    introduced server and client components, and Hydration

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,10 @@
+import Counter from "./Counter"
+
 export default function Home() {
   return (
     <div>
       <h1>Next.js is fun!</h1>
+      <Counter />
     </div>
   )
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,8 @@ export default function Footer() {
   return (
     <footer className="bg-gray-200 text-center py-4 mt-auto">
       <p>
-        &copy; {new Date().getFullYear()} - My Next.js App - Built with love
-        using Next.js
+        &copy; {new Date().getFullYear()} - My Next.js App - Built with ❤️ using
+        Next.js 16
       </p>
     </footer>
   )
